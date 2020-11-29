6fe6b2007475ed4d73f251231e4dfe24e48f4dd1
Amaya <guillaume.le-berre@epitech.eu>
2020-11-29 16:11:06 +0100

    [ADD] basic crit CI

---
+++ tests/AVTests.cpp
@@ -0,0 +1,28 @@
+/*
+** PERSONAL PROJECT, 2020
+** UWU
+** File description:
+** test_crit
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+#include <cstdlib>
+
+#include <Core.hpp>
+
+#define ASSERT_INT(a, b) cr_assert_eq(a, b, "Expected %d, got %d\n", b, a)
+
+Test(void, void)
+{
+    Core c;
+    std::vector<std::string> v;
+
+    ASSERT_INT(0, c.run(v));
+
+    v.push_back("a.out");
+
+    ASSERT_INT(1, c.run(v));
+}
+
