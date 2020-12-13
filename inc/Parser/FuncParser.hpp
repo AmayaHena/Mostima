76b4304de3b6262b7ab155ad7e37b45d37e36fda
AmayaHena <guillaume.le-berre@epitech.eu>
2020-12-13 20:34:46 +0100

    [ADD] func parser class

---
+++ inc/Parser/FuncParser.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** Mostima [WSL: Ubuntu-18.04]
+** File description:
+** FuncParser
+*/
+
+#pragma once
+
+namespace Parser {
+
+    class FuncParser {
+
+        public:
+
+            FuncParser() = default;
+            ~FuncParser() = default;
+
+        private:
+
+    };
+
+}
---
+++ src/Parser/FuncParser.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2020
+** Mostima [WSL: Ubuntu-18.04]
+** File description:
+** FuncParser
+*/
+
+#include "FuncParser.hpp"
+
+namespace Parser {
+
+}
