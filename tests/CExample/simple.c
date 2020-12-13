4b547a0da8a4c4cc33a5590c4911b30144936d5a
AmayaHena <guillaume.le-berre@epitech.eu>
2020-12-13 20:40:02 +0100

    [ADD] simple test file

---
+++ tests/CExample/simple.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** Mostima [WSL: Ubuntu-18.04]
+** File description:
+** simple
+*/
+
+#include <stdio.h>
+
+void helloWorld()
+{
+    printf("Hello world");
+}
+
+int iterate(int i)
+{
+    return i++;
+}
