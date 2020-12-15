4c2a073a6fae0b49428882ef1778851f119ee48c
AmayaHena <guillaume.le-berre@epitech.eu>
2020-12-15 18:45:34 +0100

    [ADD] placeholder for executor

---
+++ inc/Process/Executor.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** Mostima [WSL: Ubuntu-18.04]
+** File description:
+** Excutor
+*/
+
+#pragma once
+
+namespace Process {
+
+    class Excutor {
+
+        public:
+
+            Excutor() = default;
+            ~Excutor() = default;
+
+        private:
+
+    };
+
+}
---
+++ src/Process/Executor.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2020
+** Mostima [WSL: Ubuntu-18.04]
+** File description:
+** Excutor
+*/
+
+#include "Executor.hpp"
+
+namespace Process {
+
+}
